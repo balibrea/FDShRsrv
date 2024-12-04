d11af9f467c17011bcabec15ed535af28e1e1af2
Yosel de Jesus Balibrea Lastre <yosel.balibrea@gmail.com>
2024-12-04 03:39:39 -0300

    dates

--- server.py
+++ server.py
@@ -1,6 +1,24 @@
 from flask import Flask, render_template
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
+
+def shift_days(init_date, end_date):
+    start = datetime.strptime(init_date, "%Y-%m-%d")
+    end = datetime.strptime(end_date, "%Y-%m-%d")
+
+    days = []
+
+    now = start  
+    while now <= end:
+        # Formatear la fecha y agregarla a la lista  
+        days.append(now.strftime("%m-%d"))
+        # Avanzar un día  
+        now += timedelta(days=1)
+    
+    return days
+
+print(shift_days("2024-11-20", "2024-12-7"))
 
 @app.route('/')
 def home():
