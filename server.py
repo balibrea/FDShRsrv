1d3726ee8c81465e79462cc9feadab30907ed999
Yosel de Jesus Balibrea Lastre <yosel.balibrea@gmail.com>
2024-12-09 17:59:02 -0300

    interctive...

--- server.py
+++ server.py
@@ -3,6 +3,20 @@ from datetime import datetime, timedelta
 
 app = Flask(__name__)
 
+START = "2024-11-20"
+END = "2024-11-25"
+MAX_COL = 9
+
+table_cnt = 1
+
+shift_data = {
+    'LL':[],
+    'LM':[],
+    'LA':[],
+    'CO':[],
+    'HE':[]
+}
+
 def shift_days(init_date, end_date):
     start = datetime.strptime(init_date, "%Y-%m-%d")
     end = datetime.strptime(end_date, "%Y-%m-%d")
@@ -15,14 +29,29 @@ def shift_days(init_date, end_date):
         days.append(now.strftime("%m-%d"))
         # Avanzar un día  
         now += timedelta(days=1)
+
+        # Populate data (text, color)
+        d = list(shift_data.keys())
+        t = ("", "green")
+        for i in range(5):
+            shift_data[d[i]].append(t)
     
     return days
 
-print(shift_days("2024-11-20", "2024-12-7"))
+# Get days list
+days = shift_days(START, END)
+
+# Calculate the number of tables
+if len(days) > MAX_COL:
+    table_cnt = 2
+
+# DEBUG
+print(days)
+print(shift_data)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', data=shift_data, table_cnt = table_cnt, days = days)
 
 if __name__ == '__main__':
     app.run(debug=True)
